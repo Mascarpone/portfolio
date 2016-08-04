ef5ae5acde0a12e060a4eecc864e4df4bc18cd1c
Florian Le Vern <levern.f@hotmail.fr>
2016-08-04 15:45:36 +0900

    adding flask-mail to send the contect form from the smtp enseirb-matmeca server to my google account

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import os
 from flask import Flask
+from flask.ext.mail import Mail
 
 app = Flask('website')
-app.debug = True
-app.secret_key = 'd0ntW4tch'
+app.config.update(
+	DEBUG = True,
+	SECRET_KEY = '***REMOVED***',
+	# EMAIL SETTINGS
+	MAIL_SERVER = '***REMOVED***',
+	MAIL_PORT = 465, 
+	MAIL_USE_SSL = True, 
+	MAIL_USE_TLS = False,
+	MAIL_USERNAME = '***REMOVED***',
+	MAIL_PASSWORD = '***REMOVED***',
+	MAIL_DEFAULT_SENDER = '***REMOVED***'
+    )
 
+mail = Mail(app)
 bdd = "***REMOVED***"
 
 from website.controllers import *
