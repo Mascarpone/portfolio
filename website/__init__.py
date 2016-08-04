7cd992c54edfa94dcf07c66a91ae7e403ab44a22
Florian Le Vern <levern.f@hotmail.fr>
2016-08-04 16:11:11 +0900

    protecting the vars into a separate non committed file

--- website/__init__.py
+++ website/__init__.py
@@ -3,21 +3,40 @@ import os
 from flask import Flask
 from flask.ext.mail import Mail
 
+
+# INIT THE VARS
+secret_key = ''
+mail_server = ''
+mail_port = 0
+mail_use_ssl = False
+mail_use_tls = False
+mail_username = ''
+mail_password = ''
+mail_default_sender = ''
+mail_default_recipients = ['']
+bdd = ''
+g_secret = ''
+
+try: 
+	from dev_vars import *
+except ImportError:
+	pass
+
+# APPLICATION CONFIGURATION
 app = Flask('website')
 app.config.update(
-	DEBUG = True,
-	SECRET_KEY = '***REMOVED***',
+	# APP SETTINGS
+	SECRET_KEY = secret_key,
 	# EMAIL SETTINGS
-	MAIL_SERVER = '***REMOVED***',
-	MAIL_PORT = 465, 
-	MAIL_USE_SSL = True, 
-	MAIL_USE_TLS = False,
-	MAIL_USERNAME = '***REMOVED***',
-	MAIL_PASSWORD = '***REMOVED***',
-	MAIL_DEFAULT_SENDER = '***REMOVED***'
+	MAIL_SERVER = mail_server,
+	MAIL_PORT = mail_port, 
+	MAIL_USE_SSL = mail_use_ssl, 
+	MAIL_USE_TLS = mail_use_tls,
+	MAIL_USERNAME = mail_username,
+	MAIL_PASSWORD = mail_password,
+	MAIL_DEFAULT_SENDER = mail_default_sender
     )
 
 mail = Mail(app)
-bdd = "***REMOVED***"
 
 from website.controllers import *
